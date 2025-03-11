bd4101604b9271905ab324b57e611b12d32d04d8
Diego Letelier <diegoleteliers10@gmail.com>
2025-03-11 00:27:54 -0300

    feat/added sitemap and robot for the SEO.

---
+++ app/robots.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: 'Googlebot',
+        allow: ['/'],
+        disallow: '/private/',
+      },
+      {
+        userAgent: ['Applebot', 'Bingbot'],
+        disallow: ['/'],
+      },
+    ],
+    sitemap: 'http://localhost:3000/sitemap.xml', //! Cambiar por la URL oficial del sitemap
+  }
+}
