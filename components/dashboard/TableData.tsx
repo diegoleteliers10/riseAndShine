0d48823b7b05229a19dac8b33df979f726c608dd
Diego Letelier <diegoleteliers10@gmail.com>
2025-03-11 16:03:54 -0300

    update fixing table

--- components/dashboard/TableData.tsx
+++ components/dashboard/TableData.tsx
@@ -174,159 +174,161 @@ export function TableData({ orders }: { orders: Order[] }) {
 
   return (
     <div className="flex flex-col h-[500px]">
-      <div className="relative overflow-hidden rounded-xl border border-cloud-light/20 bg-white/50 shadow-sm transition-all duration-300 hover:shadow-lg flex-grow">
-        <Table>
-          <TableHeader>
-            <TableRow className="border-cloud-light/20 hover:bg-cloud-light/5">
-              <TableHead className="text-cloud-dark/60 font-medium">ID</TableHead>
-              <TableHead className="text-cloud-dark/60 font-medium">Cliente</TableHead>
-              <TableHead className="text-cloud-dark/60 font-medium">Servicio</TableHead>
-              <TableHead className="text-cloud-dark/60 font-medium">Fecha</TableHead>
-              <TableHead className="text-cloud-dark/60 font-medium">Estado</TableHead>
-              <TableHead className="text-cloud-dark/60 font-medium text-right">Monto</TableHead>
-              <TableHead className="text-cloud-dark/60 font-medium text-right"></TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <Suspense>
-              {currentOrders.length > 0 ? currentOrders.map((order) => (
-                <TableRow key={order.id} className="border-cloud-light/20 hover:bg-cloud-light/5">
-                  <TableCell className="font-medium text-cloud-dark">{order.id}</TableCell>
-                  <TableCell className="font-medium text-cloud-dark">{order.cliente.nombre}</TableCell>
-                  <TableCell className="text-cloud-dark">{order.servicio}</TableCell>
-                  <TableCell className="text-cloud-dark">
-                    {new Date(order.fecha_servicio).toLocaleDateString('es-ES', {
-                      day: '2-digit',
-                      month: 'short'
-                    })}
-                  </TableCell>
-                  <TableCell>
-                    <Badge 
-                      className={`${statusStyles[order.estado]} border`}
-                      variant="outline"
-                    >
-                      {statusLabels[order.estado]}
-                    </Badge>
-                  </TableCell>
-                  <TableCell className="text-right font-medium text-cloud-dark">
-                    ${order.monto}
-                  </TableCell>
-                  <TableCell className="text-right">
-                    <Dialog>
-                      <DialogTrigger asChild>
-                        <Pencil width={14} height={14} className='cursor-pointer'/>
-                      </DialogTrigger>
-                      <DialogContent className="sm:max-w-[425px] bg-white text-cloud-dark">
-                        <DialogHeader className="bg-white text-cloud-dark">
-                          <DialogTitle className="text-cloud-light text-[20px]">Información del pedido</DialogTitle>
-                          <DialogDescription className="text-cloud-dark space-y-2 flex flex-col">
-                            <span className='mt-2'><b>ID Pedido: </b>{order.id}</span>
-                            <span><b>Nombre: </b>{order.cliente.nombre}</span>
-                            <span><b>Servicio: </b>{order.servicio}</span>
-                            <span><b>Fecha Pedido: </b>{new Date(order.fecha_pedido).toLocaleDateString('es-ES', {
-                              day: '2-digit',
-                              month: 'short'
-                            })}</span>
-                            <span><b>Fecha Servicio: </b>{new Date(order.fecha_servicio).toLocaleDateString('es-ES', {
-                              day: '2-digit',
-                              month: 'short'
-                            })}</span>
-                            <span><b>Estado Actual: </b>{order.estado}</span>
-                          </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                          <div className="flex gap-4 items-center justify-center">
-                            <Label htmlFor="name" className="text-left text-cloud-dark">
-                              Nuevo Estado
-                            </Label>
+      <div className="rounded-xl border border-cloud-light/20 bg-white/50 shadow-sm transition-all duration-300 hover:shadow-lg">
+        <div className="relative overflow-hidden flex-grow">
+          <Table>
+            <TableHeader>
+              <TableRow className="border-cloud-light/20 hover:bg-cloud-light/5">
+                <TableHead className="text-cloud-dark/60 font-medium">ID</TableHead>
+                <TableHead className="text-cloud-dark/60 font-medium">Cliente</TableHead>
+                <TableHead className="text-cloud-dark/60 font-medium">Servicio</TableHead>
+                <TableHead className="text-cloud-dark/60 font-medium">Fecha</TableHead>
+                <TableHead className="text-cloud-dark/60 font-medium">Estado</TableHead>
+                <TableHead className="text-cloud-dark/60 font-medium text-right">Monto</TableHead>
+                <TableHead className="text-cloud-dark/60 font-medium text-right"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <Suspense>
+                {currentOrders.length > 0 ? currentOrders.map((order) => (
+                  <TableRow key={order.id} className="border-cloud-light/20 hover:bg-cloud-light/5">
+                    <TableCell className="font-medium text-cloud-dark">{order.id}</TableCell>
+                    <TableCell className="font-medium text-cloud-dark">{order.cliente.nombre}</TableCell>
+                    <TableCell className="text-cloud-dark">{order.servicio}</TableCell>
+                    <TableCell className="text-cloud-dark">
+                      {new Date(order.fecha_servicio).toLocaleDateString('es-ES', {
+                        day: '2-digit',
+                        month: 'short'
+                      })}
+                    </TableCell>
+                    <TableCell>
+                      <Badge 
+                        className={`${statusStyles[order.estado]} border`}
+                        variant="outline"
+                      >
+                        {statusLabels[order.estado]}
+                      </Badge>
+                    </TableCell>
+                    <TableCell className="text-right font-medium text-cloud-dark">
+                      ${order.monto}
+                    </TableCell>
+                    <TableCell className="text-right">
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Pencil width={14} height={14} className='cursor-pointer'/>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-[425px] bg-white text-cloud-dark">
+                          <DialogHeader className="bg-white text-cloud-dark">
+                            <DialogTitle className="text-cloud-light text-[20px]">Información del pedido</DialogTitle>
+                            <DialogDescription className="text-cloud-dark space-y-2 flex flex-col">
+                              <span className='mt-2'><b>ID Pedido: </b>{order.id}</span>
+                              <span><b>Nombre: </b>{order.cliente.nombre}</span>
+                              <span><b>Servicio: </b>{order.servicio}</span>
+                              <span><b>Fecha Pedido: </b>{new Date(order.fecha_pedido).toLocaleDateString('es-ES', {
+                                day: '2-digit',
+                                month: 'short'
+                              })}</span>
+                              <span><b>Fecha Servicio: </b>{new Date(order.fecha_servicio).toLocaleDateString('es-ES', {
+                                day: '2-digit',
+                                month: 'short'
+                              })}</span>
+                              <span><b>Estado Actual: </b>{order.estado}</span>
+                            </DialogDescription>
+                          </DialogHeader>
+                          <div className="grid gap-4 py-4">
+                            <div className="flex gap-4 items-center justify-center">
+                              <Label htmlFor="name" className="text-left text-cloud-dark">
+                                Nuevo Estado
+                              </Label>
 
-                            <Select onValueChange={(e)=>{
-                              setNewStatus(e);
-                            }}>
-                              <SelectTrigger className="w-[180px]">
-                                <SelectValue placeholder={statusLabels[order.estado]} />
-                              </SelectTrigger>
-                              <SelectContent>
-                                <SelectGroup>
-                                  <SelectLabel className="text-cloud-dark">Estado</SelectLabel>
-                                  <SelectItem value="pendiente">Pendiente</SelectItem>
-                                  <SelectItem value="en-progreso">En progreso</SelectItem>
-                                  <SelectItem value="realizado">Realizado</SelectItem>
-                                </SelectGroup>
-                              </SelectContent>
-                            </Select>
+                              <Select onValueChange={(e)=>{
+                                setNewStatus(e);
+                              }}>
+                                <SelectTrigger className="w-[180px]">
+                                  <SelectValue placeholder={statusLabels[order.estado]} />
+                                </SelectTrigger>
+                                <SelectContent>
+                                  <SelectGroup>
+                                    <SelectLabel className="text-cloud-dark">Estado</SelectLabel>
+                                    <SelectItem value="pendiente">Pendiente</SelectItem>
+                                    <SelectItem value="en-progreso">En progreso</SelectItem>
+                                    <SelectItem value="realizado">Realizado</SelectItem>
+                                  </SelectGroup>
+                                </SelectContent>
+                              </Select>
+                            </div>
                           </div>
-                        </div>
-                        <DialogFooter className="bg-white">
-                          <Button type="submit" onClick={()=>{
-                            updateStatus(order.id,newStatus);
-                          }} className="text-white bg-cloud hover:bg-cloud-dark">Actualizar Estado</Button>
-                        </DialogFooter>
-                      </DialogContent>
-                    </Dialog>
-                  </TableCell>
-                </TableRow>
-              )) : (
-                <div className="flex items-center justify-center w-auto h-auto">
-                  <span>No se encontraron pedidos</span>
-                </div>
-              )}
-            </Suspense>
-          </TableBody>
-        </Table>
-      </div>
-
-      {/* Pagination Controls */}
-      <div className="flex items-center justify-between px-4 py-4 border-t border-cloud-light/20">
-        <div className="flex items-center gap-4">
-          <span className="text-sm text-cloud-dark">
-            Página {currentPage} de {totalPages}
-          </span>
+                          <DialogFooter className="bg-white">
+                            <Button type="submit" onClick={()=>{
+                              updateStatus(order.id,newStatus);
+                            }} className="text-white bg-cloud hover:bg-cloud-dark">Actualizar Estado</Button>
+                          </DialogFooter>
+                        </DialogContent>
+                      </Dialog>
+                    </TableCell>
+                  </TableRow>
+                )) : (
+                  <div className="flex items-center justify-center w-auto h-auto">
+                    <span>No se encontraron pedidos</span>
+                  </div>
+                )}
+              </Suspense>
+            </TableBody>
+          </Table>
         </div>
-        
-        <div className="relative">
-          <input
-            type="text"
-            placeholder="Buscar por cliente o servicio..."
-            defaultValue={searchParams.get('search') || ''}
-            onChange={(e) => handleSearch(e.target.value)}
-            className="w-[500px] px-4 py-2.5 pl-10 text-sm border-2 rounded-lg border-cloud-light/30 focus:outline-none focus:border-cloud-dark/40 transition-colors duration-200"
-          />
-          <Search className="absolute left-3 top-3 h-5 w-5 text-cloud-dark/40" />
-        </div>
-        
-        <div className="flex items-center gap-2">
-          <Button
-            variant="outline"
-            size="sm"
-            asChild
-            disabled={currentPage === 1}
-            className={`px-2 ${currentPage === 1 ? 'opacity-50 cursor-not-allowed pointer-events-none' : ''}`}
-          >
-            <Link 
-              replace 
-              scroll={false}
-              href={currentPage === 1 ? '#' : createPageUrl(currentPage - 1)}
-            >
-              <ChevronLeft className="h-4 w-4" />
-            </Link>
-          </Button>
 
-          <Button
-            variant="outline"
-            size="sm"
-            asChild
-            disabled={currentPage === totalPages}
-            className={`px-2 ${currentPage === totalPages ? 'opacity-50 cursor-not-allowed pointer-events-none' : ''}`}
-          >
-            <Link 
-              replace 
-              scroll={false}
-              href={currentPage === totalPages ? '#' : createPageUrl(currentPage + 1)}
+        {/* Pagination Controls */}
+        <div className="flex items-center justify-between px-4 py-4 border-t border-cloud-light/20">
+          <div className="flex items-center gap-4">
+            <span className="text-sm text-cloud-dark">
+              Página {currentPage} de {totalPages}
+            </span>
+          </div>
+          
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Buscar por cliente o servicio..."
+              defaultValue={searchParams.get('search') || ''}
+              onChange={(e) => handleSearch(e.target.value)}
+              className="w-[500px] px-4 py-2.5 pl-10 text-sm border-2 rounded-lg border-cloud-light/30 focus:outline-none focus:border-cloud-dark/40 transition-colors duration-200"
+            />
+            <Search className="absolute left-3 top-3 h-5 w-5 text-cloud-dark/40" />
+          </div>
+          
+          <div className="flex items-center gap-2">
+            <Button
+              variant="outline"
+              size="sm"
+              asChild
+              disabled={currentPage === 1}
+              className={`px-2 ${currentPage === 1 ? 'opacity-50 cursor-not-allowed pointer-events-none' : ''}`}
             >
-              <ChevronRight className="h-4 w-4" />
-            </Link>
-          </Button>
+              <Link 
+                replace 
+                scroll={false}
+                href={currentPage === 1 ? '#' : createPageUrl(currentPage - 1)}
+              >
+                <ChevronLeft className="h-4 w-4" />
+              </Link>
+            </Button>
+
+            <Button
+              variant="outline"
+              size="sm"
+              asChild
+              disabled={currentPage === totalPages}
+              className={`px-2 ${currentPage === totalPages ? 'opacity-50 cursor-not-allowed pointer-events-none' : ''}`}
+            >
+              <Link 
+                replace 
+                scroll={false}
+                href={currentPage === totalPages ? '#' : createPageUrl(currentPage + 1)}
+              >
+                <ChevronRight className="h-4 w-4" />
+              </Link>
+            </Button>
+          </div>
         </div>
       </div>
     </div>
